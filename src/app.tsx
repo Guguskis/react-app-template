0a10965d4988905127f32a1e9c30adc3cfd2af6d
Matas Liutikas <matasliutikas@gmail.com>
2019-09-11 22:04:58 +0300

    Changed project name in package.json

--- src/app.tsx
+++ src/app.tsx
@@ -2,8 +2,6 @@ import React, { useEffect } from "react";
 import useAxios from "axios-hooks";
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 
-import "primereact/resources/primereact.min.css";
-import "primeicons/primeicons.css";
 import "./app.less";
 
 const App = () => {
